72733191624276ae46253faab43e318021096f8d
tanakaGit <100071732+dopeCoded@users.noreply.github.com>
2024-10-29 22:40:45 +0900

    finished 02

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 	"time"
 
@@ -50,7 +51,6 @@ func realMain() error {
 	// NOTE: 新しいエンドポイントの登録はrouter.NewRouterの内部で行うようにする
 	mux := router.NewRouter(todoDB)
 
-	// TODO: サーバーをlistenする
-
-	return nil
+	// HTTP サーバーを起動する
+	return http.ListenAndServe(port, mux)
 }
